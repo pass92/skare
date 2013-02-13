1f7a9fa0e7efd2a318c358d2aab736422c5c54d2
Acer <alex@ubuntu.(none)>
2013-02-13 20:40:39 +0100

    little refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
   def authenticate
     #current_user = params[:auth_user]
     @user = User.find_by_email(params[:email])
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to action: :show
+    if !@user.nil?
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to action: :show
+      else
+        redirect_to controller: :welcome, :action => :index
+      end
     else
       redirect_to controller: :welcome, :action => :index
     end
