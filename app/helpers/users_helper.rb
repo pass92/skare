8e57989c4e75f911400e08945fa8ce54790712a8
Acer <alex@ubuntu.(none)>
2013-02-22 17:36:03 +0100

    i dont know

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, dim)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.username, class: 'img-rounded', style: 'width:100px; heigth:100px')
+    image_tag(gravatar_url, alt: user.username, class: 'img-rounded', style: 'width:'+dim.to_s+'px; heigth:'+dim.to_s+'px')
   end
 end
